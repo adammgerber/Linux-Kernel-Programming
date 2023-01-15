1998e133cfb74c8dda021297e348fd872d221c71
dima <agerber7777@gmail.com>
2023-01-15 19:17:32 +0200

    initial

---
+++ mymodule.c
@@ -0,0 +1,23 @@
+#include <linux/module.h>
+#include <linux/init.h>
+
+/* Meta information: some distributions only load licensed modules */
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Adam Gerber GNU/LINUX");
+MODULE_DESCRIPTION("hello world Linux Kernel Module");
+
+
+/*This function is called when the module is loaded into the kernel */
+static int __init ModileInit(void){
+	printk("Hello, Kernel!\n");
+	return 0;
+}
+
+/*This function is called when the module is removed from the kernel*/
+static void __exit ModuleExit(void) {
+	printk("Goodbye, Kernel");
+}
+
+module_init(ModuleInit);
+module_exit(ModuleExit);
+
